6312368db2d5f2c47fb37fd27791989bbd5af2fe
Fede <ftedinur@itba.edu.ar>
2016-07-26 22:50:24 -0300

    switched to PortAudio

--- src/main.cpp
+++ src/main.cpp
@@ -1,289 +1,176 @@
+/** @file paex_sine.c
+@ingroup examples_src
+@brief Play a sine wave for several seconds.
+@author Ross Bencina <rossb@audiomulch.com>
+@author Phil Burk <philburk@softsynth.com>
+*/
 /*
- * Copyright (c) 2015 Andrew Kelley
- *
- * This file is part of libsoundio, which is MIT licensed.
- * See http://opensource.org/licenses/MIT
- */
+* $Id: paex_sine.c 1752 2011-09-08 03:21:55Z philburk $
+*
+* This program uses the PortAudio Portable Audio Library.
+* For more information see: http://www.portaudio.com/
+* Copyright (c) 1999-2000 Ross Bencina and Phil Burk
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files
+* (the "Software"), to deal in the Software without restriction,
+* including without limitation the rights to use, copy, modify, merge,
+* publish, distribute, sublicense, and/or sell copies of the Software,
+* and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+* ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
-#include <soundio/soundio.h>
-
+/*
+* The text above constitutes the entire PortAudio license; however,
+* the PortAudio community also makes the following non-binding requests:
+*
+* Any person wishing to distribute modifications to the Software is
+* requested to send the modifications to the original developer so that
+* they can be incorporated into the canonical version. It is also
+* requested that these non-binding requests be included along with the
+* license above.
+*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
 #include <math.h>
+#include "portaudio.h"
 
-static int usage(char *exe) {
-    fprintf(stderr, "Usage: %s [options]\n"
-            "Options:\n"
-            "  [--backend dummy|alsa|pulseaudio|jack|coreaudio|wasapi]\n"
-            "  [--device id]\n"
-            "  [--raw]\n"
-            "  [--name stream_name]\n"
-            "  [--latency seconds]\n"
-            "  [--sample-rate hz]\n"
-            , exe);
-    return 1;
+#define NUM_SECONDS   (5)
+#define SAMPLE_RATE   (44100)
+#define FRAMES_PER_BUFFER  (64)
+
+#ifndef M_PI
+#define M_PI  (3.14159265)
+#endif
+
+#define TABLE_SIZE   (200)
+typedef struct
+{
+	float sine[TABLE_SIZE];
+	int left_phase;
+	int right_phase;
+	char message[20];
+}
+paTestData;
+
+/* This routine will be called by the PortAudio engine when audio is needed.
+** It may called at interrupt level on some machines so don't do anything
+** that could mess up the system like calling malloc() or free().
+*/
+static int patestCallback(const void *inputBuffer, void *outputBuffer,
+	unsigned long framesPerBuffer,
+	const PaStreamCallbackTimeInfo* timeInfo,
+	PaStreamCallbackFlags statusFlags,
+	void *userData)
+{
+	paTestData *data = (paTestData*)userData;
+	float *out = (float*)outputBuffer;
+	unsigned long i;
+
+	(void)timeInfo; /* Prevent unused variable warnings. */
+	(void)statusFlags;
+	(void)inputBuffer;
+
+	for (i = 0; i<framesPerBuffer; i++)
+	{
+		*out++ = data->sine[data->left_phase];  /* left */
+		*out++ = data->sine[data->right_phase];  /* right */
+		data->left_phase += 1;
+		if (data->left_phase >= TABLE_SIZE) data->left_phase -= TABLE_SIZE;
+		data->right_phase += 3; /* higher pitch so we can distinguish left and right. */
+		if (data->right_phase >= TABLE_SIZE) data->right_phase -= TABLE_SIZE;
+	}
+
+	return paContinue;
 }
 
-static void write_sample_s16ne(char *ptr, double sample) {
-    int16_t *buf = (int16_t *)ptr;
-    double range = (double)INT16_MAX - (double)INT16_MIN;
-    double val = sample * range / 2.0;
-    *buf = val;
+/*
+* This routine is called by portaudio when playback is done.
+*/
+static void StreamFinished(void* userData)
+{
+	paTestData *data = (paTestData *)userData;
+	printf("Stream Completed: %s\n", data->message);
 }
 
-static void write_sample_s32ne(char *ptr, double sample) {
-    int32_t *buf = (int32_t *)ptr;
-    double range = (double)INT32_MAX - (double)INT32_MIN;
-    double val = sample * range / 2.0;
-    *buf = val;
-}
+/*******************************************************************/
+int main(void);
+int main(void)
+{
+	PaStreamParameters outputParameters;
+	PaStream *stream;
+	PaError err;
+	paTestData data;
+	int i;
 
-static void write_sample_float32ne(char *ptr, double sample) {
-    float *buf = (float *)ptr;
-    *buf = sample;
-}
 
-static void write_sample_float64ne(char *ptr, double sample) {
-    double *buf = (double *)ptr;
-    *buf = sample;
-}
+	printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
 
-static void (*write_sample)(char *ptr, double sample);
-static const double PI = 3.14159265358979323846264338328;
-static double seconds_offset = 0.0;
-static volatile bool want_pause = false;
-static void write_callback(struct SoundIoOutStream *outstream, int frame_count_min, int frame_count_max) {
-    double float_sample_rate = outstream->sample_rate;
-    double seconds_per_frame = 1.0 / float_sample_rate;
-    struct SoundIoChannelArea *areas;
-    int err;
+	/* initialise sinusoidal wavetable */
+	for (i = 0; i<TABLE_SIZE; i++)
+	{
+		data.sine[i] = (float)sin(((double)i / (double)TABLE_SIZE) * M_PI * 2.);
+	}
+	data.left_phase = data.right_phase = 0;
 
-    int frames_left = frame_count_max;
+	err = Pa_Initialize();
+	if (err != paNoError) goto error;
 
-    for (;;) {
-        int frame_count = frames_left;
-        if ((err = soundio_outstream_begin_write(outstream, &areas, &frame_count))) {
-            fprintf(stderr, "unrecoverable stream error: %s\n", soundio_strerror(err));
-            exit(1);
-        }
+	outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+	if (outputParameters.device == paNoDevice) {
+		fprintf(stderr, "Error: No default output device.\n");
+		goto error;
+	}
+	outputParameters.channelCount = 2;       /* stereo output */
+	outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
+	outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
+	outputParameters.hostApiSpecificStreamInfo = NULL;
 
-        if (!frame_count)
-            break;
+	err = Pa_OpenStream(
+		&stream,
+		NULL, /* no input */
+		&outputParameters,
+		SAMPLE_RATE,
+		FRAMES_PER_BUFFER,
+		paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+		patestCallback,
+		&data);
+	if (err != paNoError) goto error;
 
-        const struct SoundIoChannelLayout *layout = &outstream->layout;
+	sprintf(data.message, "No Message");
+	err = Pa_SetStreamFinishedCallback(stream, &StreamFinished);
+	if (err != paNoError) goto error;
 
-        double pitch = 440.0;
-        double radians_per_second = pitch * 2.0 * PI;
-        for (int frame = 0; frame < frame_count; frame += 1) {
-            double sample = sinf((seconds_offset + frame * seconds_per_frame) * radians_per_second);
-            for (int channel = 0; channel < layout->channel_count; channel += 1) {
-                write_sample(areas[channel].ptr, sample);
-                areas[channel].ptr += areas[channel].step;
-            }
-        }
-        seconds_offset += seconds_per_frame * frame_count;
+	err = Pa_StartStream(stream);
+	if (err != paNoError) goto error;
 
-        if ((err = soundio_outstream_end_write(outstream))) {
-            if (err == SoundIoErrorUnderflow)
-                return;
-            fprintf(stderr, "unrecoverable stream error: %s\n", soundio_strerror(err));
-            exit(1);
-        }
+	printf("Play for %d seconds.\n", NUM_SECONDS);
+	Pa_Sleep(NUM_SECONDS * 1000);
 
-        frames_left -= frame_count;
-        if (frames_left <= 0)
-            break;
-    }
+	err = Pa_StopStream(stream);
+	if (err != paNoError) goto error;
 
-    soundio_outstream_pause(outstream, want_pause);
-}
+	err = Pa_CloseStream(stream);
+	if (err != paNoError) goto error;
 
-static void underflow_callback(struct SoundIoOutStream *outstream) {
-    static int count = 0;
-    fprintf(stderr, "underflow %d\n", count++);
-}
+	Pa_Terminate();
+	printf("Test finished.\n");
 
-int main(int argc, char **argv) {
-    char *exe = argv[0];
-    enum SoundIoBackend backend = SoundIoBackendNone;
-    char *device_id = NULL;
-    bool raw = false;
-    char *stream_name = NULL;
-    double latency = 0.0;
-    int sample_rate = 0;
-    for (int i = 1; i < argc; i += 1) {
-        char *arg = argv[i];
-        if (arg[0] == '-' && arg[1] == '-') {
-            if (strcmp(arg, "--raw") == 0) {
-                raw = true;
-            } else {
-                i += 1;
-                if (i >= argc) {
-                    return usage(exe);
-                } else if (strcmp(arg, "--backend") == 0) {
-                    if (strcmp(argv[i], "dummy") == 0) {
-                        backend = SoundIoBackendDummy;
-                    } else if (strcmp(argv[i], "alsa") == 0) {
-                        backend = SoundIoBackendAlsa;
-                    } else if (strcmp(argv[i], "pulseaudio") == 0) {
-                        backend = SoundIoBackendPulseAudio;
-                    } else if (strcmp(argv[i], "jack") == 0) {
-                        backend = SoundIoBackendJack;
-                    } else if (strcmp(argv[i], "coreaudio") == 0) {
-                        backend = SoundIoBackendCoreAudio;
-                    } else if (strcmp(argv[i], "wasapi") == 0) {
-                        backend = SoundIoBackendWasapi;
-                    } else {
-                        fprintf(stderr, "Invalid backend: %s\n", argv[i]);
-                        return 1;
-                    }
-                } else if (strcmp(arg, "--device") == 0) {
-                    device_id = argv[i];
-                } else if (strcmp(arg, "--name") == 0) {
-                    stream_name = argv[i];
-                } else if (strcmp(arg, "--latency") == 0) {
-                    latency = atof(argv[i]);
-                } else if (strcmp(arg, "--sample-rate") == 0) {
-                    sample_rate = atoi(argv[i]);
-                } else {
-                    return usage(exe);
-                }
-            }
-        } else {
-            return usage(exe);
-        }
-    }
-
-    struct SoundIo *soundio = soundio_create();
-    if (!soundio) {
-        fprintf(stderr, "out of memory\n");
-        return 1;
-    }
-
-    int err = (backend == SoundIoBackendNone) ?
-        soundio_connect(soundio) : soundio_connect_backend(soundio, backend);
-
-    if (err) {
-        fprintf(stderr, "Unable to connect to backend: %s\n", soundio_strerror(err));
-        return 1;
-    }
-
-    fprintf(stderr, "Backend: %s\n", soundio_backend_name(soundio->current_backend));
-
-    soundio_flush_events(soundio);
-
-    int selected_device_index = -1;
-    if (device_id) {
-        int device_count = soundio_output_device_count(soundio);
-        for (int i = 0; i < device_count; i += 1) {
-            struct SoundIoDevice *device = soundio_get_output_device(soundio, i);
-            bool select_this_one = strcmp(device->id, device_id) == 0 && device->is_raw == raw;
-            soundio_device_unref(device);
-            if (select_this_one) {
-                selected_device_index = i;
-                break;
-            }
-        }
-    } else {
-        selected_device_index = soundio_default_output_device_index(soundio);
-    }
-
-    if (selected_device_index < 0) {
-        fprintf(stderr, "Output device not found\n");
-        return 1;
-    }
-
-    struct SoundIoDevice *device = soundio_get_output_device(soundio, selected_device_index);
-    if (!device) {
-        fprintf(stderr, "out of memory\n");
-        return 1;
-    }
-
-    fprintf(stderr, "Output device: %s\n", device->name);
-
-    if (device->probe_error) {
-        fprintf(stderr, "Cannot probe device: %s\n", soundio_strerror(device->probe_error));
-        return 1;
-    }
-
-    struct SoundIoOutStream *outstream = soundio_outstream_create(device);
-    if (!outstream) {
-        fprintf(stderr, "out of memory\n");
-        return 1;
-    }
-
-    outstream->write_callback = write_callback;
-    outstream->underflow_callback = underflow_callback;
-    outstream->name = stream_name;
-    outstream->software_latency = latency;
-    outstream->sample_rate = sample_rate;
-
-    if (soundio_device_supports_format(device, SoundIoFormatFloat32NE)) {
-        outstream->format = SoundIoFormatFloat32NE;
-        write_sample = write_sample_float32ne;
-    } else if (soundio_device_supports_format(device, SoundIoFormatFloat64NE)) {
-        outstream->format = SoundIoFormatFloat64NE;
-        write_sample = write_sample_float64ne;
-    } else if (soundio_device_supports_format(device, SoundIoFormatS32NE)) {
-        outstream->format = SoundIoFormatS32NE;
-        write_sample = write_sample_s32ne;
-    } else if (soundio_device_supports_format(device, SoundIoFormatS16NE)) {
-        outstream->format = SoundIoFormatS16NE;
-        write_sample = write_sample_s16ne;
-    } else {
-        fprintf(stderr, "No suitable device format available.\n");
-        return 1;
-    }
-
-    if ((err = soundio_outstream_open(outstream))) {
-        fprintf(stderr, "unable to open device: %s", soundio_strerror(err));
-        return 1;
-    }
-
-    fprintf(stderr, "Software latency: %f\n", outstream->software_latency);
-    fprintf(stderr,
-            "'p\\n' - pause\n"
-            "'u\\n' - unpause\n"
-            "'P\\n' - pause from within callback\n"
-            "'c\\n' - clear buffer\n"
-            "'q\\n' - quit\n");
-
-    if (outstream->layout_error)
-        fprintf(stderr, "unable to set channel layout: %s\n", soundio_strerror(outstream->layout_error));
-
-    if ((err = soundio_outstream_start(outstream))) {
-        fprintf(stderr, "unable to start device: %s\n", soundio_strerror(err));
-        return 1;
-    }
-
-    for (;;) {
-        soundio_flush_events(soundio);
-        int c = getc(stdin);
-        if (c == 'p') {
-            fprintf(stderr, "pausing result: %s\n",
-                    soundio_strerror(soundio_outstream_pause(outstream, true)));
-        } else if (c == 'P') {
-            want_pause = true;
-        } else if (c == 'u') {
-            want_pause = false;
-            fprintf(stderr, "unpausing result: %s\n",
-                    soundio_strerror(soundio_outstream_pause(outstream, false)));
-        } else if (c == 'c') {
-            fprintf(stderr, "clear buffer result: %s\n",
-                    soundio_strerror(soundio_outstream_clear_buffer(outstream)));
-        } else if (c == 'q') {
-            break;
-        } else if (c == '\r' || c == '\n') {
-            // ignore
-        } else {
-            fprintf(stderr, "Unrecognized command: %c\n", c);
-        }
-    }
-
-    soundio_outstream_destroy(outstream);
-    soundio_device_unref(device);
-    soundio_destroy(soundio);
-    return 0;
+	return err;
+error:
+	Pa_Terminate();
+	fprintf(stderr, "An error occured while using the portaudio stream\n");
+	fprintf(stderr, "Error number: %d\n", err);
+	fprintf(stderr, "Error message: %s\n", Pa_GetErrorText(err));
+	return err;
 }
